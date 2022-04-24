c2d590306462469c42b19f4865a8d98ca30d08e1
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-04-24 21:41:55 +0530

    updating files

---
+++ src/routes/navgation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from "react/cjs/react.production.min"
+import { Outlet,Link } from "react-router-dom"
+
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+
+import './navigation.styles.scss';
+
+const Navigation =()=>{
+    return (
+      <Fragment>
+        <div className="navigation">
+         <Link className="logo-container" to=''>
+         <CrownLogo classNmae='logo'/>
+         </Link>
+         
+         <div className="nav-links-container">
+        <Link className="nav-link" to='/shop'>
+            SHOP
+        </Link>
+         </div>
+        </div>
+        <Outlet/>
+        {/* This outlet will now, in turn, render everything else inside */}
+  
+      </Fragment>
+    )
+  }
+
+export default Navigation
