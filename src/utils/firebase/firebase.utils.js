abad1bd1ef5175d6e730687290982cdeb447438d
shalnsingh@outlook.com <shalnsingh@outlook.com>
2022-04-25 18:34:08 +0530

    updated firebase utils and brought in firebase libaries

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -42,4 +42,21 @@ const firebaseConfig = {
 
       const userSnapshot = await getDoc(userDocRef);
       console.log(userSnapshot.exists());
+      if(!userSnapshot.exists()){
+        const {displayName,email}=userAuth;
+        const createdAt=new Date();
+    
+  
+    try{
+        await setDoc(userDocRef,{
+            displayName,
+            email,
+            createdAt
+        });
+    }
+    catch(error){
+    }  
+    }
+    return userDocRef
   }
+ 
